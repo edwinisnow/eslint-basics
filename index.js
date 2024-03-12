13101433fd4453ee27ff082f9a37560cac364dcc
EDWIN GEORGE LAZER <edwin04@gmail.com>
2024-03-12 18:48:47 +0000

    lint plugins

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-const express = require("express")
-const x:string = 1
+const express = require('express')
+
+const x = "1";
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('hello')
+    process.exit(1)
+
+})
+
+app.listen(3000, () => {
+    console.log('App is listening on port 3000')
+})
+
+const obj = {
+    hello: function () {
+        console.log('hello');
+    }
+}
